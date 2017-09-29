804e4ee0cc711cc21d7c5973cd620f8935135e4b
lienthealien <dreamcodesoft@gmail.com>
2017-09-29 22:17:34 +0300

    updated local kotlin version

--- app/src/main/java/hackathon/com/salemovedroid/activity/BaseActivity.java
+++ app/src/main/java/hackathon/com/salemovedroid/activity/BaseActivity.java
@@ -1,8 +1,28 @@
 package hackathon.com.salemovedroid.activity;
 
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.util.logging.Logger;
+
 /**
  * Created by John on 9/29/2017.
  */
-import android.support.v7.app.ActionBarActivity;
-public class BaseActivity extends ActionBarActivity {
+
+
+
+
+public class BaseActivity extends AppCompatActivity {
+    protected Logger log = Logger.getLogger(BaseActivity.class.getName());
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
 }
