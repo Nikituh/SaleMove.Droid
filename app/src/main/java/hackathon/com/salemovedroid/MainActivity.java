c60eed5adf878b1f26fd730134e61e36e49beffb
Aare Undo <aare@carto.com>
2017-09-29 21:35:14 +0300

    salemove api implementation; token read from assets

--- app/src/main/java/hackathon/com/salemovedroid/MainActivity.java
+++ app/src/main/java/hackathon/com/salemovedroid/MainActivity.java
@@ -1,16 +1,48 @@
 package hackathon.com.salemovedroid;
 
+import android.os.NetworkOnMainThreadException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 import hackathon.com.salemovedroid.networking.Networking;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Networking networking;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        new Networking().doStuff();
+        networking = new Networking();
+
+        String token = "";
+        try {
+            token = readFromAssets("token.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Networking.setToken(token);
+
+        networking.doStuff();
+    }
+
+    public String readFromAssets(String filename) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(getAssets().open(filename)));
+
+        // do reading, usually loop until end of file reading
+        StringBuilder sb = new StringBuilder();
+        String mLine = reader.readLine();
+        while (mLine != null) {
+            sb.append(mLine); // process line
+            mLine = reader.readLine();
+        }
+        reader.close();
+        return sb.toString();
     }
 }
--- app/src/main/java/networking/Networking.kt
+++ app/src/main/java/networking/Networking.kt
@@ -1,12 +1,8 @@
 package hackathon.com.salemovedroid.networking
 
+import android.util.Log
 import com.github.kittinunf.fuel.Fuel
 import com.github.kittinunf.fuel.core.FuelManager
-import com.github.kittinunf.fuel.httpGet
-import com.github.kittinunf.result.Result
-import com.github.kittinunf.result.getAs
-import org.jetbrains.anko.doAsync
-import java.net.URL
 
 /**
  * Created by aareundo on 29/09/2017.
@@ -17,17 +13,34 @@ class Networking {
      * WebRCT: https://webrtc.org/native-code/android/
      */
 
-    val url = "http://httpbin.org/get"
+    companion object {
+        @JvmStatic var token = ""
+    }
+
+    private val site_id = "4acb779c-a1d8-4d38-a4e6-402b7ed1453d"
+    private val base_url = "https://api.beta.salemove.com/"
+    private val operator_url = base_url + "operators?site_ids[]=$site_id&include_offline=false"
 
     fun doStuff() {
 
-        Fuel.get("http://httpbin.org/get").response { request, response, result ->
+        val headers = mapOf(
+                "Authorization" to "Token $token",
+                "Accept" to "application/vnd.salemove.v1+json"
+        )
+
+        FuelManager.instance.baseHeaders = headers
+
+        Fuel.get(operator_url).response { request, response, result ->
             println(request)
             println(response)
-            val (bytes, error) = result
-            if (bytes != null) {
-                println(bytes)
-            }
+
+            result.fold(success = { json ->
+                val string = json.sortedArray().toString()
+
+                Log.d("qdp success", string)
+            }, failure = { error ->
+                Log.e("qdp error", error.toString())
+            })
         }
 
     }
