196e7cd5a72ece956f940ce80e10897ba9d7be0f
lienthealien <dreamcodesoft@gmail.com>
2017-09-29 22:45:08 +0300

    code update

--- app/src/main/java/hackathon/com/salemovedroid/activity/BaseActivity.java
+++ app/src/main/java/hackathon/com/salemovedroid/activity/BaseActivity.java
@@ -3,6 +3,8 @@ package hackathon.com.salemovedroid.activity;
 /**
  * Created by John on 9/29/2017.
  */
-import android.support.v7.app.ActionBarActivity;
-public class BaseActivity extends ActionBarActivity {
+
+import android.support.v7.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
 }
