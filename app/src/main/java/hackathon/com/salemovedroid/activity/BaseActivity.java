76d5a440bdd496a5631f48926d7dc9bed42f44ed
lienthealien <dreamcodesoft@gmail.com>
2017-09-29 20:58:28 +0300

    main structure

---
+++ app/src/main/java/hackathon/com/salemovedroid/activity/BaseActivity.java
@@ -0,0 +1,8 @@
+package hackathon.com.salemovedroid.activity;
+
+/**
+ * Created by John on 9/29/2017.
+ */
+import android.support.v7.app.ActionBarActivity;
+public class BaseActivity extends ActionBarActivity {
+}
---
+++ app/src/main/java/hackathon/com/salemovedroid/activity/OperatorsActivity.java
@@ -0,0 +1,8 @@
+package hackathon.com.salemovedroid.activity;
+
+/**
+ * Created by John on 9/29/2017.
+ */
+
+public class OperatorsActivity {
+}
